884b2cd5d9961ae5324ed505c9be6d545789c98f
East-Sound <kareemhekal19@gmail.com>
2025-02-08 16:35:16 +0200

    change UI

--- lib/Auth/Login/View/login_screen.dart
+++ lib/Auth/Login/View/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:online_exam_app/Shared/custom_button.dart';
 import 'package:online_exam_app/Shared/custom_password_text_field.dart';
 import 'package:online_exam_app/Shared/custom_text_field.dart';
+import 'package:online_exam_app/config.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Config().init(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -31,11 +33,13 @@ class _LoginScreenState extends State<LoginScreen> {
         elevation: 0,
       ),
       body: SingleChildScrollView(
+
         physics: BouncingScrollPhysics(),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisSize: MainAxisSize.min,
           children: [
+            SizedBox(height: Config.screenHight! * 0.01),
             /* -------------------------------------------------------------------------- */
             /*                                 Email Field                                */
             /* -------------------------------------------------------------------------- */
@@ -56,46 +60,49 @@ class _LoginScreenState extends State<LoginScreen> {
             /* -------------------------------------------------------------------------- */
             /*                         Remember Me & Forget Password                     */
             /* -------------------------------------------------------------------------- */
-            Row(
-              children: [
-                /* ------------------------------ Remember Me ------------------------------ */
-                Checkbox(
-                  activeColor: Color(0xff02369C),
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  },
-                ),
-                Text(
-                  "Remember me",
-                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
-                ),
-                Spacer(),
-
-                /* ---------------------------- Forget Password ---------------------------- */
-
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      print("Forget password Button Pressed");
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  /* ------------------------------ Remember Me ------------------------------ */
+                  Checkbox(
+                    activeColor: Color(0xff02369C),
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
                     },
-                    child: Text(
-                      "Forget password?",
-                      style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.black,
-                        decorationThickness: 2,
-                        fontSize: 13,
-                        fontWeight: FontWeight.w400,
+                  ),
+                  Text(
+                    "Remember me",
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
+                  ),
+                  Spacer(),
+
+                  /* ---------------------------- Forget Password ---------------------------- */
+
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        print("Forget password Button Pressed");
+                      },
+                      child: Text(
+                        "Forget password?",
+                        style: TextStyle(
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.black,
+                          decorationThickness: 2,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             /* -------------------------------------------------------------------------- */
             SizedBox(height: 30),
