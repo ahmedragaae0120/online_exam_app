272e5cccb04e1d4de3b6173e3e13136176c7cc60
اسمك <ahmedbasha01208@gmail.com>
2025-02-24 20:52:02 +0200

    fix: remove subject images paths in AssetsManager file

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_single_quotes
 class AssetsManager {
   AssetsManager._();
-  
+
   static const String resourceClock = "assets/clock.png";
 
   static const String resourceLanguageExam = "assets/language_exam.png";
@@ -9,13 +9,4 @@ class AssetsManager {
   static const String resourceMathExam = "assets/math_exam.png";
 
   static const String resourceSandClock = "assets/sand-clock.png";
-
-  static const String resourceSubjectArt = "assets/subject/art.png";
-
-  static const String resourceSubjectLanguage = "assets/subject/Language.png";
-
-  static const String resourceSubjectMath = "assets/subject/math.png";
-
-  static const String resourceSubjectScience = "assets/subject/Science.png";
 }
-
